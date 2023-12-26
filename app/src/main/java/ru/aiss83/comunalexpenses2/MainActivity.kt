f07a89f6c97da46ed78b32a9d3187ec6cc7f2f0b
aiss83 <aiss83@ya.ru>
2023-12-26 17:40:23 +0500

    Added DataStore

--- app/src/main/java/ru/aiss83/comunalexpenses2/MainActivity.kt
+++ app/src/main/java/ru/aiss83/comunalexpenses2/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.aiss83.comunalexpenses2
 
 import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import ru.aiss83.comunalexpenses2.data.DataStoreManagerFactory
+import ru.aiss83.comunalexpenses2.data.SettingsDataSource
 import ru.aiss83.comunalexpenses2.screens.MainScreen
 import ru.aiss83.comunalexpenses2.ui.theme.ComunalExpenses2Theme
+
+private const val USER_PREFERENCES_NAME = "user_preferences"
+
+private val Context.dataSource by preferencesDataStore(USER_PREFERENCES_NAME)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +37,7 @@ class MainActivity : ComponentActivity() {
                     val owner = LocalViewModelStoreOwner.current
 
                     owner?.let {
+                        val prefs = DataStoreManagerFactory().create(SettingsDataSource.PREFERENCES_DATA_STORE, dataSource)
                         val viewModel: ResourcesDataViewModel = viewModel(
                             it,
                             "ResourcesDataViewModel",
--- app/src/main/java/ru/aiss83/comunalexpenses2/data/DataStoreManagerFactory.kt
+++ app/src/main/java/ru/aiss83/comunalexpenses2/data/DataStoreManagerFactory.kt
@@ -1,10 +1,17 @@
 package ru.aiss83.comunalexpenses2.data
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+
 enum class SettingsDataSource {
     PREFERENCES_DATA_STORE
 }
 class DataStoreManagerFactory {
-    fun create(source: SettingsDataSource): DataStoreManager {
-        return DataStoreManagerImpl()
+    fun create(source: SettingsDataSource, dataStore: DataStore<Preferences>): DataStoreManager {
+        when (source) {
+            SettingsDataSource.PREFERENCES_DATA_STORE -> return DataStoreManagerImpl(dataStore)
+            else -> throw IllegalArgumentException("Unknown DataSource object")
+        }
+
     }
 }
