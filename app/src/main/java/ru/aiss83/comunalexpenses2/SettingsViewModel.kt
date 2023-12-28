197fbd7c615615eda763f1d08f20b44c332ac141
aiss83 <aiss83@ya.ru>
2023-12-28 18:08:43 +0500

    Adding settings view model.

---
+++ app/src/main/java/ru/aiss83/comunalexpenses2/SettingsViewModel.kt
@@ -0,0 +1,10 @@
+package ru.aiss83.comunalexpenses2
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel(dataStore: DataStore<Preferences>): ViewModel() {
+
+    val
+}
