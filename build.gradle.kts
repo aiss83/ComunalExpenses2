7ecf959c1efe1db799dc06fddc2b89b5be2bc818
aiss83 <ais@ais-systems.ru>
2026-05-08 17:18:39 +0500

    Gradle updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.1.1" apply false
-    id("com.android.library") version "9.1.1" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.1.21" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
